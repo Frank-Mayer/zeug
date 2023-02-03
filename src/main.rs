4fc14fd073de787559f7970f11427d2cadef85d8
Frank-Mayer <git@frank-mayer.io>
2023-02-03 14:37:13 +0100

    format

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 mod youtube;
 
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    // build application with a single route
+    const PORT: u32 = 3000u32;
+
     let app = Router::new()
         .route("/youtube/live", get(youtube::live));
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    println!("Listening on http://127.0.0.1:{}", PORT);
+
+    axum::Server::bind(&format!("0.0.0.0:{}", PORT).parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
