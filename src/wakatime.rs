72a1359fc0b3e557055e1586aa5cd2aa47d7bf4f
Frank-Mayer <git@frank-mayer.io>
2023-02-06 00:26:42 +0100

    call wakatime api

--- src/wakatime.rs
+++ src/wakatime.rs
@@ -1,5 +1,36 @@
 use axum::http::{Response, StatusCode};
+use serde::{Deserialize, Serialize};
 use simple_xml_builder::XMLElement;
+extern crate regex;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct WakatimeLanguages {
+    data: WakatimeLanguagesData,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct WakatimeLanguage {
+    name: String,
+    total_seconds: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct WakatimeLanguagesData {
+    is_up_to_date: bool,
+    languages: Vec<WakatimeLanguage>,
+    status: String,
+}
+
+async fn fetch_wakatime_languages() -> Result<WakatimeLanguagesData, reqwest::Error> {
+    let resp_obj: WakatimeLanguages = reqwest::Client::new()
+        .get("https://wakatime.com/api/v1/users/tsukinoko/insights/languages/last_7_days")
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    return Ok(resp_obj.data);
+}
 
 fn make_background() -> XMLElement {
     let mut rect_el = XMLElement::new("rect");
@@ -44,6 +75,23 @@ pub async fn wakatime() -> Response<String> {
     svg_el.add_child(make_style());
     svg_el.add_child(make_background());
     svg_el.add_child(make_title("Most Used Languages Last Week"));
+
+    let mut lang_list_el = XMLElement::new("ul");
+    match fetch_wakatime_languages().await {
+        Ok(data) => {
+            for lang in data.languages {
+                let mut lang_el = XMLElement::new("li");
+                lang_el.add_text(lang.name);
+                lang_list_el.add_child(lang_el);
+            }
+        }
+        Err(err) => {
+            let mut err_el = XMLElement::new("li");
+            err_el.add_text(err.to_string());
+            lang_list_el.add_child(err_el);
+        }
+    }
+    svg_el.add_child(lang_list_el);
 
     Response::builder()
         .status(StatusCode::OK)
